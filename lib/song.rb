b2be8cfd89a608274fc75b991bb4b912e2255798
Walter Aab <aabwalter@yahoo.com>
2020-11-08 22:23:13 -0600

    commit tree d1e891a9339019c25b3eb9f75b08d3facccaedb9

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,60 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{ |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    #if self.find_by_name(name)                       #both versions of the logic gate perform the same task and will pass the test
+    #  self.find_by_name(name)                        
+    #else
+    #  self.create_by_name(name)
+    #end
+    self.find_by_name(name) || self.create_by_name(name) 
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |song| song.name }
+    #self.all.sort_by(&:name)                         #this is another way to write sort by and asking machine to retun each name while it is parsing
+  end
+
+  def self.new_from_filename(filename)
+    song = self.new
+    split_filename = filename.chomp(".mp3").split(" - ")
+    song.name = split_filename[1]
+    song.artist_name = split_filename[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.create
+    split_filename = filename.chomp(".mp3").split(" - ")
+    song.name = split_filename[1]
+    song.artist_name = split_filename[0]
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
